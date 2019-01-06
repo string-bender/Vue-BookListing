c8ab6594ebe2eabb274478e7d836c0a7e07b49e2
string-bender <apaulsson@outlook.com>
2019-01-06 14:59:24 +0100

    Completed the module

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <!-- <hello-world></hello-world> -->
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import BookList from './components/BookList';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    // HelloWorld,
+       BookList,
   },
 };
 </script>
